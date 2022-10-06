517b0383224f49eb1f38809a4adc1c182dc34e86
MillanGarcia <millan.garcia.mata@gmail.com>
2022-10-06 18:06:37 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 /*import "bootstrap";
 import "./style.css";*/ //para npm, para live-server no hace falta
+
 window.onload = function() {
   document.body.style.backgroundColor = "green";
 
@@ -15,12 +16,51 @@ window.onload = function() {
   carta.style.backgroundColor = "white";
   carta.style.display = "in-block";
   carta.style.borderRadius = "5px";
-  carta.style.padding = "30px";
+  carta.style.padding = "3%";
+
+  function changeSize() {
+    let height1 = document.getElementById("hcard");
+
+    let number1 = height1.value + "px";
+
+    let fontnumberh = (80 * height1.value) / 300;
+
+    /*let fontsize1 = fontnumberh + "px";*/
+
+    /*carta.style.fontSize = fontsize1;*/
+    carta.style.height = number1;
+
+    let width1 = document.getElementById("wcard");
+    let number2 = width1.value + "px";
+    let fontnumberw = (80 * width1.value) / 180;
+
+    if (fontnumberh >= fontnumberw) {
+      /*let percentage = fontnumberw / fontnumberh;
+      let result = percentage * fontnumberh;*/
+      let fontsize2 = fontnumberh + "px";
+
+      carta.style.fontSize = fontsize2;
+    } else {
+      let percentage = fontnumberh / fontnumberw;
+      let result = percentage * fontnumberw;
+      let fontsize2 = result + "px";
+      carta.style.fontSize = fontsize2;
+    }
+    /*let fontsize2 = fontnumberw + "px";*/
+
+    carta.style.width = number2;
+  }
+
+  document.getElementById("btn").onclick = function() {
+    changeSize();
+  };
+
+  /*setInterval(changeSize, 3000);*/
 
   const part1 = document.getElementById("parte1");
   part1.style.width = "inherit";
-  part1.style.maxHeight = "33%";
-  part1.style.marginTop = "-20px";
+  part1.style.height = "33%";
+  /*part1.style.marginTop = "-9.96%";*/
   /*part1.style.float = "50px";*/
 
   part1.style.textAlign = "left";
@@ -28,15 +68,15 @@ window.onload = function() {
   part2.style.width = "inherit";
   part2.style.margin = "auto";
   part2.style.textAlign = "center";
-  part2.style.maxHeight = "33%";
-  part2.style.paddingTop = "20px";
+  part2.style.height = "33%";
+  /*part2.style.paddingTop = "10%";*/
 
   const part3 = document.getElementById("parte3");
   part3.style.width = "inherit";
   part3.style.textAlign = "right";
-  part3.style.marginRight = "50px";
-  part3.style.maxHeight = "33%";
-  part3.style.paddingTop = "30px";
+  part3.style.marginRight = "5%";
+  part3.style.height = "33%";
+  /*part3.style.paddingTop = "10%";*/
 
   const icons = ["♦", "♥", "♠", "♣"];
 
@@ -64,4 +104,9 @@ window.onload = function() {
     part3.innerHTML = icons[numero1];
   }
   CardGen1();
+  document.getElementById("btn2").onclick = function() {
+    CardGen1();
+  };
+
+  setInterval(CardGen1, 10000);
 };
